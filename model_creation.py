dd01244b9f87c8abd186109c89501ccc60e80577
dougtheeconomist <douglass.hart@outlook.com>
2021-01-09 15:56:34 -0800

    added code to gen forecast, save results to arrays

--- model_creation.py
+++ model_creation.py
@@ -29,3 +29,28 @@ mdata.index = pd.DatetimeIndex(monthly)
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~Model Creation~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 maw = VAR(mdata, freq='m')
+
+# Traditionally with 6 lags:
+results = maw.fit(6)
+# Or to autoselect lag order based on info criterion:
+results = maw.fit(maxlags=12, ic='bic')
+
+# Note that we have to specify the “initial value” for the forecast:
+lag_order = results.k_ar # this equals the number of lags used to build model
+
+# To gen forecast of six steps out:
+results.forecast(mdata.values[-lag_order:], 5)
+
+# provides forecast and intervals 
+results.forecast_interval(mdata.values[-lag_order:], 6)
+
+# So to save as arrays:
+point_fcast = results.forecast_interval(mdata.values[-lag_order:], 5)[0]
+lower_bounds = results.forecast_interval(mdata.values[-lag_order:], 5)[1]
+upper_bounds = results.forecast_interval(mdata.values[-lag_order:], 5)[2]
+
+# Can plot with:
+results.plot_forecast(6)
+# THIS IS NOT A GOOD LOOKING GRAPH!
+# Will want to implement own custom graphing function for variables of interest
+# like with housing forecasting Deep Learning model
