ce80a62e0b5d227e1c9060bde596bf93b9dbc09b
dougtheeconomist <douglass.hart@outlook.com>
2021-01-20 14:23:03 -0800

    added get calibration function

--- src/model_creation.py
+++ src/model_creation.py
@@ -54,3 +54,86 @@ results.plot_forecast(6)
 # THIS IS NOT A GOOD LOOKING GRAPH!
 # Will want to implement own custom graphing function for variables of interest
 # like with housing forecasting Deep Learning model
+
+def get_calibration_results(df, n_results):
+    '''
+    Runs VAR code and saves resulting predictions to lists, then drops most recent row of data and repeats n_results 
+    times. Coded for save calibration data for 6 period forecast. 
+    inputs:
+        df: dataframe containing data for use in VAR model
+        n_results: int, number of periods backwards to run the model and retain results, must be greater than 6
+    
+    '''
+    dfc = pd.DataFrame(df,copy=True)
+    fm1 = []
+    fm2 = []
+    fm3 = []
+    fm4 = []
+    fm5 = []
+    fm6 = []
+
+    um1 = []
+    um2 = []
+    um3 = []
+    um4 = []
+    um5 = []
+    um6 = []
+
+    lm1 = []
+    lm2 = []
+    lm3 = []
+    lm4 = []
+    lm5 = []
+    lm6 = []
+    
+    for i in range((n_results+5)):
+        dfc.drop(dfc.tail(1).index,inplace=True)
+        maw = VAR(dfc, freq='m')
+
+        fm1.insert(0,point_fcast[0][0])
+        fm2.insert(0,point_fcast[1][0])
+        fm3.insert(0,point_fcast[2][0])
+        fm4.insert(0,point_fcast[3][0])
+        fm5.insert(0,point_fcast[4][0])
+        fm6.insert(0,point_fcast[5][0])
+
+        lm1.insert(0,point_fcast[0][1])
+        lm2.insert(0,point_fcast[1][1])
+        lm3.insert(0,point_fcast[2][1])
+        lm4.insert(0,point_fcast[3][1])
+        lm5.insert(0,point_fcast[4][1])
+        lm6.insert(0,point_fcast[5][1])
+
+
+        um1.insert(0,point_fcast[0][2])
+        um2.insert(0,point_fcast[1][2])
+        um3.insert(0,point_fcast[2][2])
+        um4.insert(0,point_fcast[3][2])
+        um5.insert(0,point_fcast[4][2])
+        um6.insert(0,point_fcast[5][2])
+    # Then to trim lists to proper intervals
+    fm1 = fm1[:-5]
+    fm2 = fm2[1:-4]
+    fm3 = fm3[2:-3]
+    fm4 = fm4[3:-2]
+    fm5 = fm5[4:-2]
+    fm6 = fm6[5:]
+
+    um1 = um1[:-5]
+    um2 = um2[1:-4]
+    um3 = um3[2:-3]
+    um4 = um4[3:-2]
+    um5 = um5[4:-1]
+    um6 = um6[5:]
+
+    lm1 = lm1[:-5]
+    lm2 = lm2[1:-4]
+    lm3 = lm3[2:-3]
+    lm4 = lm4[3:-2]
+    lm5 = lm5[4:-1]
+    lm6 = lm6[5:]
+    
+    out = pd.DataFrame((list(zip(fm1, fm2, fm3, fm4, fm5, fm6, lm1, lm2, lm3, lm4, lm5, lm6, um1, um2, um3, um4, um5, um6))), 
+               columns =['p1p', 'p2p', 'p3p', 'p4p', 'p5p', 'p6p', 'p1l', 'p2l', 'p3l', 'p4l', 'p5l', 'p6l', 'p1u', 'p2u', 'p3u', 'p4u', 'p5u', 'p6u'],index=df.index[-n_results:])
+    out['actual'] = df['pcgdp'][-n_results:]
+    return out
