85e62cc334718471d5bd36ee46eb05f6dc2a2316
dougtheeconomist <douglass.hart@outlook.com>
2021-02-05 14:37:33 -0800

    bug corrections in functions

--- src/model_creation.py
+++ src/model_creation.py
@@ -92,7 +92,7 @@ def get_calibration_data(df, n_results):
     lm4 = []
     lm5 = []
     lm6 = []
-    
+
     for i in range((n_results+5)):
         mdata.drop(mdata.tail(1).index,inplace=True)
         maw = VAR(mdata, freq='m')
@@ -102,7 +102,7 @@ def get_calibration_data(df, n_results):
         point_fcast = results.forecast_interval(mdata.values[-lag_order:], 6)[0]
         lower_bounds = results.forecast_interval(mdata.values[-lag_order:], 6)[1]
         upper_bounds = results.forecast_interval(mdata.values[-lag_order:], 6)[2]
-
+        
         fm1.insert(0,point_fcast[0][0])
         fm2.insert(0,point_fcast[1][0])
         fm3.insert(0,point_fcast[2][0])
@@ -125,26 +125,26 @@ def get_calibration_data(df, n_results):
         um5.insert(0,point_fcast[4][2])
         um6.insert(0,point_fcast[5][2])
     # Then to trim lists to proper intervals
-    fm1 = fm1[:-5]
-    fm2 = fm2[1:-4]
-    fm3 = fm3[2:-3]
-    fm4 = fm4[3:-2]
-    fm5 = fm5[4:-1]
-    fm6 = fm6[5:]
+    fm1 = fm1[5:]
+    fm2 = fm2[4:-1]
+    fm3 = fm3[3:-2]
+    fm4 = fm4[2:-3]
+    fm5 = fm5[1:-4]
+    fm6 = fm6[:-5]
 
-    um1 = um1[:-5]
-    um2 = um2[1:-4]
-    um3 = um3[2:-3]
-    um4 = um4[3:-2]
-    um5 = um5[4:-1]
-    um6 = um6[5:]
+    um1 = um1[5:]
+    um2 = um2[4:-1]
+    um3 = um3[3:-2]
+    um4 = um4[2:-3]
+    um5 = um5[1:-4]
+    um6 = um6[:-5]
 
-    lm1 = lm1[:-5]
-    lm2 = lm2[1:-4]
-    lm3 = lm3[2:-3]
-    lm4 = lm4[3:-2]
-    lm5 = lm5[4:-1]
-    lm6 = lm6[5:]
+    lm1 = lm1[5:]
+    lm2 = lm2[4:-1]
+    lm3 = lm3[3:-2]
+    lm4 = lm4[2:-3]
+    lm5 = lm5[1:-4]
+    lm6 = lm6[:-5]
     
     out = pd.DataFrame((list(zip(fm1, fm2, fm3, fm4, fm5, fm6, lm1, lm2, lm3, lm4, lm5, lm6, um1, um2, um3, um4, um5, um6))), 
                columns =['p1p', 'p2p', 'p3p', 'p4p', 'p5p', 'p6p', 'p1l', 'p2l', 'p3l', 'p4l', 'p5l', 'p6l', 'p1u', 'p2u', 'p3u', 'p4u', 'p5u', 'p6u'],index=df.index[-n_results:])
@@ -186,12 +186,20 @@ def calibration_check(actual,upper,lower, bias_as_percent=False):
         return calibration, bias
 
 
+# def report_calibration(df,n):
+#     report_list = [get_calibration_results(df,n)]
+#     print('Month One Calibration: ', calibration_check(report_list[0],report_list[6] ,report_list[12]))
+#     print('Month Two Calibration: ', calibration_check(report_list[1],report_list[7] ,report_list[13]))
+#     print('Month Three Calibration: ', calibration_check(report_list[2],report_list[8] ,report_list[14]))
+#     print('Month Four Calibration: ', calibration_check(report_list[3],report_list[9] ,report_list[15]))
+#     print('Month Five Calibration: ', calibration_check(report_list[4],report_list[10] ,report_list[16]))
+#     print('Month Six Calibration: ', calibration_check(report_list[5],report_list[11] ,report_list[17]))
+
 def report_calibration(df,n):
     report_list = [get_calibration_results(df,n)]
-    print('Month One Calibration: ', calibration_check(report_list[0],report_list[6] ,report_list[12]))
-    print('Month Two Calibration: ', calibration_check(report_list[1],report_list[7] ,report_list[13]))
-    print('Month Three Calibration: ', calibration_check(report_list[2],report_list[8] ,report_list[14]))
-    print('Month Four Calibration: ', calibration_check(report_list[3],report_list[9] ,report_list[15]))
-    print('Month Five Calibration: ', calibration_check(report_list[4],report_list[10] ,report_list[16]))
-    print('Month Six Calibration: ', calibration_check(report_list[5],report_list[11] ,report_list[17]))
-
+    print('Month One Calibration: ', calibration_check(report_list.fm1, report_list.lm1, report_list.um1))
+    print('Month Two Calibration: ', calibration_check(report_list.fm2, report_list.lm2, report_list.um2))
+    print('Month Three Calibration: ', calibration_check(report_list.fm3, report_list.lm3, report_list.um3))
+    print('Month Four Calibration: ', calibration_check(report_list.fm4, report_list.lm4, report_list.um4))
+    print('Month Five Calibration: ', calibration_check(report_list.fm5, report_list.lm5, report_list.um5))
+    print('Month Six Calibration: ', calibration_check(report_list.fm6, report_list.lm6, report_list.um6))
